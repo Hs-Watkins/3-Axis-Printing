b82f507215cac8683febff18fe7cfe5e6c560775
Hs-Watkins <78350370+Hs-Watkins@users.noreply.github.com>
2024-02-17 15:14:37 -0500

    Slides 5-8, notes in code, variable velocities.

--- 3 Axis Printing/Program.cs
+++ 3 Axis Printing/Program.cs
@@ -15,48 +15,36 @@ namespace KDC101Console
 {
     class Program
     {
-        SerialPort port;
+        // declare the serial port
+        static SerialPort port;
         static void Main(string[] args)
         {
             // Find the Devices and Begin Communicating with them via USB
             // Enter the serial number for your device
-            string serialNo1 = "27505282";
-            string serialNo2 = "27505360";
-            string serialNo3 = "27505370";
-
-
+            string serialNo1 = "27505282"; // x
+            string serialNo2 = "27505360"; // y
+            string serialNo3 = "27505370"; // z
 
             DeviceManagerCLI.BuildDeviceList();
 
-
-
-            // This creates an instance of KCubeDCServo class, passing in the Serial 
-            //Number parameter.  
+            // Creates an instance of KCubeDCServo class, passing in the Serial number parameter.  
             KCubeDCServo device1 = KCubeDCServo.CreateKCubeDCServo(serialNo1);
             KCubeDCServo device2 = KCubeDCServo.CreateKCubeDCServo(serialNo2);
             KCubeDCServo device3 = KCubeDCServo.CreateKCubeDCServo(serialNo3);
-
-
 
             // We tell the user that we are opening connection to the device. 
             Console.WriteLine("Opening device {0}", serialNo1);
             Console.WriteLine("Opening device {0}", serialNo2);
             Console.WriteLine("Opening device {0}", serialNo3);
 
-
-
-            // This connects to the device. 
+            // Connects to the device. 
             device1.Connect(serialNo1);
-
-
 
             // Wait for the device settings to initialize. We ask the device to 
             // throw an exception if this takes more than 5000ms (5s) to complete. 
             device1.WaitForSettingsInitialized(5000);
 
-
-
-            // Same for Device 2.
+            // Same for Device 2 and 3
             device2.Connect(serialNo2);
             device2.WaitForSettingsInitialized(5000);
 
@@ -78,13 +66,9 @@ namespace KDC101Console
 
             // This starts polling the device at intervals of 250ms (0.25s).
 
-
-
             device1.StartPolling(250);
             device2.StartPolling(250);
             device3.StartPolling(250);
-
-
 
             // We are now able to Enable the device otherwise any move is ignored. 
             // You should see a physical response from your controller. 
@@ -93,59 +77,57 @@ namespace KDC101Console
             device3.EnableDevice();
             Console.WriteLine("Devices Enabled");
 
-
-
-
-
             // Needs a delay to give time for the device to be enabled. 
             Thread.Sleep(500);
 
+            Console.WriteLine("Is the slide removed?");
+            string answer1 = Console.ReadLine();
+            Console.WriteLine("Now Homing");
 
-
-            // Home both actuators at once  
+            // Home all actuators at once  
             Thread Home1Thread = new Thread(() => Home1(device1));
             Thread Home2Thread = new Thread(() => Home2(device2));
             Thread Home3Thread = new Thread(() => Home2(device3));
-            Console.WriteLine("Actuators are Homing");
             Home1Thread.Start();
             Home2Thread.Start();
             Home3Thread.Start();
-
-
 
             // Wait for the threads to complete
             Home1Thread.Join();
             Home2Thread.Join();
             Home3Thread.Join();
 
-
-
             //device1.SetJogVelocityParams();
 
-
-
-            // What are the Points we want to hit?
-            // decimal[] Xpositions = { 8.973m, 8.864m, 8.551m, 8.067m, 7.464m, 6.808m, 6.169m, 5.618m, 5.214m, 5m, 5m, 5.214m, 5.618m, 6.169m, 6.808m, 7.464m, 8.067m, 8.551m, 8.864m, 8.973m };
-            // decimal[] Ypositions = { 1.993m, 2.643m, 3.222m, 3.668m, 3.932m, 3.986m, 3.825m, 3.465m, 2.945m, 2.322m, 1.664m, 1.041m, 0.522m, 0.162m, 0m, 0.054m, 0.319m, 0.765m, 1.344m, 1.993m };
-
-
+            // move z axis so you can put slide in place
             Thread MoveZThreadInit = new Thread(() => MoveZ(device3, 20,10));
             MoveZThreadInit.Start();
             MoveZThreadInit.Join();
-
+            Console.WriteLine("Make sure power supply is on");
             Console.WriteLine("Are You Prepared?");
             string answer = Console.ReadLine();
             Console.WriteLine("Now Proceeding");
 
+            //decimal[] Xpositions = {0, };
+            //decimal[] Ypositions = {15, };
+            //decimal[] Zpositions = {7.3m, };
+
+            // positional positions - don't start any access on 0  
+
             decimal[] Xpositions = {0,0,5,5,10,10,15,15,20,20, };
             decimal[] Ypositions = {15,25,25,15,15,25,25,15,15,25, };
-            decimal[] Zpositions = {7.3m, 7.3m, 7.3m, 7.3m, 7.3m, 7.3m, 7.3m, 7.3m, 7.3m, 7.3m, };
-            byte[] PValues = { 0, 50, 250, 200, 50, 50, 50, 50, 50, 50, };
-            int[] VCMValues = { 0, 50, 50, 50, 50, 50, 50, 50, 50, 50,  };
-            int[] Velocities = { 10, 10, 10, 10, 10, 10, 10, 10, 10,10,  };
+            decimal[] Zpositions = {2.75m, 2.75m, 2.75m, 2.75m, 2.75m, 2.75m, 2.75m, 2.75m, 2.75m, 2.75m, };
 
-            //SerialPort port;
-            //port = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+            // power 1/0, start on 0
+            byte[] PValues = { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, };
+            //int[] VCMValues = { 0, 50, 50, 50, 50, 50, 50, 50, 50, 50,  };
+            int[] Velocities = { 5, 5, 10, 10, 15, 15, 20, 20, 20,20,  };
+
+            // add check to see if lengths are same
+
+
+            //SerialPort port
+            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
 
             // Iterate through XPositions and YPositions Simultaneously and Synchronously
             Console.WriteLine("Actuator is Moving");
@@ -156,8 +138,15 @@ namespace KDC101Console
                 Thread MoveYThread = new Thread(() => MoveY(device2, Ypositions[i], Velocities[i]));
                 Thread MoveZThread = new Thread(() => MoveZ(device3, Zpositions[i], Velocities[i]));
 
-                /*int firstValue = PValues[i];
-                int secondValue = 0;
+                int pValue = PValues[i];
+                string pSend = pValue.ToString();
+
+                port.Open();
+                port.Write(pSend);
+                port.Close();
+                Console.WriteLine("Power Signal: {0}", PValues[i]);
+
+                /*int secondValue = 0;
                 int thirdValue = 0;
 
                 if (VCMValues[i] > 0)
@@ -185,9 +174,6 @@ namespace KDC101Console
                 MoveXThread.Start();
                 MoveYThread.Start();
                 MoveZThread.Start();
-                //Have it raise up to the maximum z-height
-                //look up C# equivalent to python's input command
-
 
                 // Wait for Move to Finish
                 MoveXThread.Join();
@@ -195,14 +181,16 @@ namespace KDC101Console
                 MoveZThread.Join();
             }
 
+            // turn off laser
+            port.Open();
+            port.Write("0");
+            port.Close();
+            Console.WriteLine("Laser Off");
 
             // Raise head to allow you to remove slide
-
             Thread MoveZThreadEnd = new Thread(() => MoveZ(device3, 20, 10));
             MoveZThreadEnd.Start();
             MoveZThreadEnd.Join();
-
-
 
             //Change the Diode Power Output
             //Make a loop that prints serial output that runs simultaneously with the motion stage
@@ -217,16 +205,12 @@ namespace KDC101Console
             device2.StopPolling();
             device3.StopPolling();
 
-
-
             // Shut down controller using Disconnect() to close comms
-            // Then the used library
             device1.ShutDown();
             device2.ShutDown();
             device3.ShutDown();
 
-
-            Console.WriteLine("Complete. Press any key to exit");
+            Console.WriteLine("Your print is finished. Press any key to exit");
             Console.ReadKey();
         }
         static void MoveX(KCubeDCServo device1, decimal Xposition, int Velocities)
